44dda6cc566e84bc7e9132eb09d9ec17c1c4bcb4
Philippe Canavarro <pgcanavarro@gmail.com>
2022-05-02 02:14:00 +0100

    Default of 10 phrases if no options is passed.

--- index.js
+++ index.js
@@ -1,103 +1,94 @@
-const fetch = require("node-fetch"),
-  slugify = require("slugify"),
-  cheerio = require("cheerio"),
-  iconv = require("iconv-lite");
+const fetch = require('node-fetch'),
+	slugify = require('slugify'),
+	cheerio = require('cheerio'),
+	iconv = require('iconv-lite');
 
-  const baseUrl = "https://www.pensador.com/";
+const baseUrl = 'https://www.pensador.com/';
 
-  module.exports = async (options) => {
-    if (options === undefined) {
-      _throw("A search term must be defined");
-    }
-    // Default value for search term, Term is not mandatory anymore
-    var searchTerm = "frases_curtas";
-    // if term is set then is changed to a new value
-    if (options.term != "undefined") {
-      searchTerm = slugify(`frases de ${options.term}`, {
-        replacement: "_",
-        remove: /[*+~.()'"!:@]/g,
-        lower: true,
-      });
-    }
-  
-  let keepGoing = true;
-  let current = 1;
+module.exports = async (options) => {
+	if (typeof options == 'undefined') {
+		var options = { max: 10 };
+	}
+  // Default value for search term, Term is not mandatory anymore
+	var searchTerm = 'frases_curtas';
+  // if term is set then is changed to a new value
+	if (options.term != 'undefined') {
+		searchTerm = slugify(`frases de ${options.term}`, {
+			replacement: '_',
+			remove: /[*+~.()'"!:@]/g,
+			lower: true,
+		});
+	}
 
-  let phrases = [];
+	let keepGoing = true;
+	let current = 1;
 
-  while (keepGoing) {
-    let contentPage = await fetchPage(searchTerm, current);
-    let result = await extract(contentPage);
+	let phrases = [];
 
-    phrases.push(...result.phrases);
+	while (keepGoing) {
+		let contentPage = await fetchPage(searchTerm, current);
+		let result = await extract(contentPage);
 
-    if (options.max !== undefined && phrases.length > options.max) {
-      phrases = phrases.slice(0, options.max);
+		phrases.push(...result.phrases);
 
-      keepGoing = false;
-    }
+		if (options.max !== undefined && phrases.length > options.max) {
+			phrases = phrases.slice(0, options.max);
 
-    if (result.next === false) {
-      keepGoing = false;
-    }
+			keepGoing = false;
+		}
 
-    current = current + 1;
-  }
+		if (result.next === false) {
+			keepGoing = false;
+		}
 
-  return { total: phrases.length, searchTerm, phrases };
+		current = current + 1;
+	}
 
-  async function fetchPage(searchTerm, current = 1) {
-    return new Promise((resolve, reject) => {
-      fetch(`${baseUrl}/${searchTerm}/${current}`)
-        .then(res => res.arrayBuffer())
-        .then(arrayBuffer =>
-          iconv.decode(Buffer.from(arrayBuffer), "utf-8").toString()
-        )
-        .then(body => resolve(body))
-        .catch(err => reject(err));
-    });
-  }
+	return { total: phrases.length, searchTerm, phrases };
 
-  async function extract(htmlContent) {
-    return new Promise((resolve, reject) => {
-      try {
-        const phrases = [];
-        const $ = cheerio.load(htmlContent);
-        $(".thought-card").each(function(i, e) {
-          phrases.push({
-            author: $(this)
-              .find("a")
-              .first()
-              .text(),
-            text: $(this)
-              .find("p")
-              .first()
-              .text()
-              .replace(/\n/g, "")
-          });
-        });
+	async function fetchPage(searchTerm, current = 1) {
+		return new Promise((resolve, reject) => {
+			fetch(`${baseUrl}/${searchTerm}/${current}`)
+				.then((res) => res.arrayBuffer())
+				.then((arrayBuffer) =>
+					iconv.decode(Buffer.from(arrayBuffer), 'utf-8').toString(),
+				)
+				.then((body) => resolve(body))
+				.catch((err) => reject(err));
+		});
+	}
 
-        let next = false;
-        $("#paginacao").each(function(i, e) {
-          if (
-            $(this)
-              .find(".nav")
-              .last()
-              .text()
-              .includes("xima")
-          ) {
-            next = true;
-          }
-        });
+	async function extract(htmlContent) {
+		return new Promise((resolve, reject) => {
+			try {
+				const phrases = [];
+				const $ = cheerio.load(htmlContent);
+				$('.thought-card').each(function (i, e) {
+					phrases.push({
+						author: $(this).find('a').first().text(),
+						text: $(this)
+							.find('p')
+							.first()
+							.text()
+							.replace(/\n/g, ''),
+					});
+				});
 
-        resolve({ phrases, next });
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
+				let next = false;
+				$('#paginacao').each(function (i, e) {
+					if ($(this).find('.nav').last().text().includes('xima')) {
+						next = true;
+					}
+				});
+
+				resolve({ phrases, next });
+			} catch (err) {
+				reject(err);
+			}
+		});
+	}
 };
 
 function _throw(m) {
-  throw m;
+	throw m;
 }
